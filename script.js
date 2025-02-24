bf82344b6b1190dc2a011306e46fa7e68ab96df6
andrewmbeckmann <andrewmbeckmann@gmail.com>
2025-02-24 12:29:51 -0500

    Better mobile support (drag, spacing), slight refactor

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 if(!localStorage.getItem("palette")) localStorage.setItem("palette",["red", "orange", "yellow", "green", "cyan", "blue", "purple", "pink", "black", "white"])
 let palette = getPalette();
-let paletteSize = 16; //will make this option between 16/32
+let canvasSize = 16; //will make this option between 16/32
 let downedMouse = false;
 let selectedColor = previousColor = "black";
 
@@ -10,10 +10,10 @@ function buildCanvas(){
     canvas.id = "canvas" 
     let pixel = document.createElement("div");
     pixel.classList.add("pixel")
-    pixel.classList.add(getPixelClass(paletteSize))
+    pixel.classList.add(getPixelClass(canvasSize))
     document.body.append(canvas);
-    for(i = 0; i < paletteSize; i++){
-        for(j = 0; j < paletteSize; j++){
+    for(i = 0; i < canvasSize; i++){
+        for(j = 0; j < canvasSize; j++){
             let newClone = pixel.cloneNode(true);
             canvas.appendChild(newClone);
             newClone.addEventListener("mouseover", () => {
@@ -35,13 +35,32 @@ function addMouseDownListeners(domObject){
     document.addEventListener("mouseup", () => {
         downedMouse = false;
     })
+    domObject.addEventListener("touchmove", (e) => {
+        let currentX = e.touches[0].clientX;
+        let currentY = e.touches[0].clientY;
+        console.log(currentX)
+        let allPixels = document.getElementsByClassName("pixel");
+        let pixelIndex = 0;
+        let pixelSize = allPixels[0].getBoundingClientRect().width;
+        console.log(pixelSize)
+        for(i = 0; i < canvasSize; i++){
+            for(j = 0; j < canvasSize; j++){
+                let currentPixel = allPixels[pixelIndex];
+                let currentPixelRect = currentPixel.getBoundingClientRect();
+                if(currentX > currentPixelRect.left && currentX < currentPixelRect.left + pixelSize && currentY > currentPixelRect.top && currentY < currentPixelRect.top + pixelSize){
+                    currentPixel.style.backgroundColor = selectedColor;
+                }
+                pixelIndex++;
+            }
+        }
+    })
 }
 
 function downloadAsPNG(){
    let canvasElem = createCanvasElem();
    canvasElem.toBlob(blob => {
     let download = document.createElement("a")
-    download.setAttribute("download", "pixelart" + paletteSize + "x" + paletteSize + ".png");
+    download.setAttribute("download", "pixelart" + canvasSize + "x" + canvasSize + ".png");
     let url = URL.createObjectURL(blob);
     download.setAttribute('href', url);
     download.click();
@@ -50,13 +69,13 @@ function downloadAsPNG(){
 
 function createCanvasElem(){
     let canvasElem = document.createElement("canvas");
-    canvasElem.height = paletteSize
-    canvasElem.width = paletteSize
+    canvasElem.height = canvasSize
+    canvasElem.width = canvasSize
     canvasContext = canvasElem.getContext("2d");
     let allPixels = document.getElementsByClassName("pixel")
     let pixelIndex = 0;
-    for(i = 0; i < paletteSize; i++){
-        for(j = 0; j < paletteSize; j++){
+    for(i = 0; i < canvasSize; i++){
+        for(j = 0; j < canvasSize; j++){
             let currentPixel = allPixels[pixelIndex]
             canvasContext.fillStyle = currentPixel.style.backgroundColor;
             if(currentPixel.style.backgroundColor === "") canvasContext.fillStyle = '#FFFFFF00'
